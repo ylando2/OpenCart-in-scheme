9361a02c73bc8d250fb0fc2ebf08f2c6cf302218
ylando2 <ylando2@gmail.com>
2012-05-09 01:06:28 +0200

    Small fix

--- template.scm
+++ template.scm
@@ -1,11 +1,13 @@
-(require-extension irregex)
-(require-extension srfi-69)
+(module template (render)
+  (import scheme chicken)
+  (use irregex srfi-69)
 
-;Temporary before we find or create a faster way. 
-(define (render str hash)
-  (irregex-replace/all 
-    '(: "<%" (* space) ($ (*? any)) (* space) "%>") 
-    str 
-    (lambda (x) 
-      (let ((str2 (irregex-match-substring x 1)))
-        (hash-table-ref/default hash str2 str2)))))
+  ;Temporary before we find or create a faster way. 
+  (define (render str hash)
+    (irregex-replace/all 
+      '(: "<%" (* space) ($ (*? any)) (* space) "%>") 
+      str 
+      (lambda (x) 
+        (let ((str2 (irregex-match-substring x 1)))
+          (hash-table-ref/default hash str2 str2)))))
+        )
